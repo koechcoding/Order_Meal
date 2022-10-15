9af4663d2cd3f84034ae38b69c8f39a358652b23
koechcoding <koechkelvin2001@gmail.com>
2022-10-15 08:12:14 +0300

    improve test coverage

---
+++ book_meal/src/components/common/Menu/Menu.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { shallow} from 'enzyme';
+import 'src/utils/bootTests'; //order matters
+import Menu from './Menu';
+import Adapter from 'enzyme-adapter-react-16';
