264405c72fccb42ac01e2f2c66d10b29c497a0d2
koechcoding <koechkelvin2001@gmail.com>
2022-10-15 07:57:52 +0300

    improve test coverage

--- book_meal/src/components/common/Menu/Menu.js
+++ book_meal/src/components/common/Menu/Menu.js
@@ -2,3 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MenuItem from './components/MenuItem';
 
+const Menu = ({ items, menu = {}, onOrder }) => (
+    <div>
+        <h5 className="text-center mt-2">{menu.name}</h5>
+        <div className="row">
+            {items.map(item => <MenuItem menu={menu} item={item} key={item.id} onOrder={onOrder} />)}
+        </div>
+    </div>
+);
+
+Menu.propTypes = {
+    items: PropTypes.array,
+    menu: PropTypes.object,
+    onOrder: PropTypes.func.isRequired
+}
+
+export default Menu;
