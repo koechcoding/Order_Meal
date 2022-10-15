8f535a1f5adb17fbc05995d892d1d1410e5c1d96
koechcoding <koechkelvin2001@gmail.com>
2022-10-15 08:18:46 +0300

    improve test coverage

--- book_meal/src/components/common/Menu/Menu.test.js
+++ book_meal/src/components/common/Menu/Menu.test.js
@@ -1,5 +1,26 @@
 import React from 'react';
-import { shallow} from 'enzyme';
-import 'src/utils/bootTests'; //order matters
+import { shallow }  from 'enzyme';
+import 'src/utils/bootTests'; // order matters!
 import Menu from './Menu';
 import Adapter from 'enzyme-adapter-react-16';
+
+const items = [{
+        id: 0,
+        name: 'name',
+        cost: 50,
+        img_url: '#'
+    },
+    {
+        id: 1,
+        name: 'name2',
+        cost: 50,
+        img_url: '#'
+    }
+]
+
+describe('<Menu />', () => {
+    it('renders correctly', () => {
+        const wrapper = shallow(<Menu items={items} onOrder={() => {}} setLoading={() => {}}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
