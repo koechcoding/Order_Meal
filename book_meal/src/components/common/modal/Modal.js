1004ac8db9a027ce6a68c996865feaf80d8ae12c
koechcoding <koechkelvin2001@gmail.com>
2022-10-16 13:36:14 +0300

    improve test coverage

--- book_meal/src/components/common/modal/Modal.js
+++ book_meal/src/components/common/modal/Modal.js
@@ -6,4 +6,33 @@ import {
     ModalBody,
     ModalFooter
 } from 'reactstrap';
-import './styles.css'
+import './styles.css';
+
+const Modal = ({ size="md", onOpened, title, body, footer, isOpen, toggle}) =>(
+    <BModal size={size} isOpen={isOpen} onOpened={onOpened} toggle={toggle} fade={false}>
+        <ModalHeader toggle={toggle}>
+            {title}
+            <button onClick={()=>toggle()} type="button" className="close">
+              <span aria-hidden="true">$times</span>
+            </button>
+        </ModalHeader>
+        <ModalBody>
+            {body}
+        </ModalBody>
+        <ModalFooter>
+            {footer}
+        </ModalFooter>
+    </BModal>
+);
+
+Modal.propTypes ={
+    isOpen: PropTypes.bool,
+    size: PropTypes.string,
+    body: PropTypes.element,
+    onOpened: PropTypes.func,
+    title: PropTypes.string,
+    footer: PropTypes.element,
+    toggle: propTypes.func.isRequired,
+}
+
+export default Modal;
