b9a1275b7810f39a2543f36002b34907c695cd48
koechcoding <koechkelvin2001@gmail.com>
2022-10-16 13:25:34 +0300

    improve test coverage

---
+++ book_meal/src/components/common/modal/Modal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    Modal as BModal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
+import './styles.css'
