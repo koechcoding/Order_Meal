b5d9b4a378ca5f0becfaddb8f244f3c51099387f
koechcoding <koechkelvin2001@gmail.com>
2022-10-16 20:07:57 +0300

    Add landing and login page

--- book_meal/src/components/views/Landing/components/Body/Body.test.js
+++ book_meal/src/components/views/Landing/components/Body/Body.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { shallow} from 'enzyme';
-import 'src/utils/bootTests';
+import { shallow }  from 'enzyme';
+import 'src/utils/bootTests'; 
 import Body from './Body';
 
-describe('<Body />', ()=>{
-    it("renders successfully", ()=>{
+describe('<Body />', () => {
+    it('renders correctly', () => {
         const wrapper = shallow(<Body/>);
         expect(wrapper).toMatchSnapshot();
     });
 });
+
