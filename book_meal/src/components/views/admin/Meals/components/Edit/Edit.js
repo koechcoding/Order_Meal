95b08368836ba0ce2b6f58e95a965c07b4f7c7fc
koechcoding <koechkelvin2001@gmail.com>
2022-10-17 06:43:05 +0300

    Improve test coverage

--- book_meal/src/components/views/admin/Meals/components/Edit/Edit.js
+++ book_meal/src/components/views/admin/Meals/components/Edit/Edit.js
@@ -61,5 +61,52 @@ class EditModal extends React.Component {
             setTimeout(this.props.toggle, 1000);
         }
         //on failure
+        const reject = (response)=>{
+            this.setState({
+                ...this.state,
+                error: response,
+                success: false,
+            });
+            this.props.setLoading(false);
+        }
+
+        this.props.setLoading(true);
+        
+        const { image, name, cost, img_url} = this.state;
+        if (image){
+            const imageUpload = {
+                file: image,
+                upload_preset: IMAGE_UPLOAD_PRESET
+            };
+
+            // delete auth headers...
+            delete axios.default.headers.common.Authorization;
+            delete axios.default.headers.common['Access-Control-Allow-Origin'];
+            // image upload...
+            axios.post(IMAGES_UPLOAD_URL, imageUpload).then(({data}) => {
+                axios.auth();
+                axios.put('/meals/${this.props.meal.id', {
+                    name,
+                    cost,
+                    img_url: data.secure_url
+                }).then(()=>{
+                    resolve
+                }).catch(({ response })=>{
+                    reject(response)
+                });
+            }).catch(({response}) => {
+                //restore headers
+                axios.auth();
+                reject(response);
+            });
+
+        }else {
+            axios.put('/meals/${this.props.meal.id}', {name, cost, img_url}).then(()=>{
+                resolve();
+            }).catch(({ response}) =>{
+                reject(response);
+            })
+        }
     }
+    
 }
