81bfcfca54080315d13b848aeb7d5c0e4af51d93
koechcoding <koechkelvin2001@gmail.com>
2022-10-17 08:48:04 +0300

    Improve test coverage

---
+++ book_meal/src/components/views/admin/Meals/components/MealsTable/MealsTable.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import 'src/utils/bootTests';
+import MealsTable from './MealsTable';
+import Adapter from 'enzyme-adapter-react-16';
+
+co
