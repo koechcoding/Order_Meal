108cec248f9e8e5d8185f0de1c7ad2f9b1686515
koechcoding <koechkelvin2001@gmail.com>
2022-10-18 12:23:28 +0300

    Improve Test Coverage

---
+++ book_meal/src/components/views/admin/Menus/components/MenusTypes/Delete.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import axios from 'src/axios';
+import PropTypes from 'prop-types';
+import { Alert, Button } from 'reactstrap';
+import Modal from 'src/components/common/Modal';
+import { singleError } from 'src/utils';
