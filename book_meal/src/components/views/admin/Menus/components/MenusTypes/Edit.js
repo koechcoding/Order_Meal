e33c7c605cc4fa623452a9fffb8796c504ab18d7
koechcoding <koechkelvin2001@gmail.com>
2022-10-18 13:08:41 +0300

    Improve Test Coverage

---
+++ book_meal/src/components/views/admin/Menus/components/MenusTypes/Edit.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'src/axios';
+import { Alert, Button, Input } from 'reactstrap';
+import Modal from 'src/components/common/Modal';
+import { singleError } from 'src/utils';
