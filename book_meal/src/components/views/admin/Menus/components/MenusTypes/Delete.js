ff1872a5ce8884eea76dd5f46713604a7152df16
koechcoding <koechkelvin2001@gmail.com>
2022-10-18 12:34:29 +0300

    Improve Test Coverage

--- book_meal/src/components/views/admin/Menus/components/MenusTypes/Delete.js
+++ book_meal/src/components/views/admin/Menus/components/MenusTypes/Delete.js
@@ -5,3 +5,33 @@ import PropTypes from 'prop-types';
 import { Alert, Button } from 'reactstrap';
 import Modal from 'src/components/common/Modal';
 import { singleError } from 'src/utils';
+
+class Delete extends React.Component {
+    state={
+        menu: {}
+    }
+
+    onDelete=()=>{
+        this.props.setLoading(true);
+        axios.delete('/menus/${this.props.menu.id}').then(()=>{
+            this.setState({
+                ...this.state,
+                toDelete: null,
+                deleteIsOpen: false,
+            });
+            this.props.setLoading(false);
+            this.props.toggle();
+            this.props.onChange();
+        }).catch(({ response }) =>{
+            this.setState({
+                ...this.state,
+                error: response,
+            });
+            this.props.setLoading(false);
+        });
+    }
+
+    render(){
+        
+    }
+}
