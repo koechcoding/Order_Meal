45c409b1a1fdd87b5eb4bdf1f3a1565696c4e933
koechcoding <koechkelvin2001@gmail.com>
2022-10-19 17:38:40 +0300

    Improve test coverage

---
+++ book_meal/src/components/views/admin/Menus/components/MenuTables/MenuTable.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import Paginator from 'src/components/common/Paginator';
+import { flattenObject, paginationInfo } from 'src/utils';
+
+import Table from 'src/components/common/Table';
+import { EntryType } from 'src/constants';
+
+class MenuItemsTable extends React.Component {
+    
+}
