37f096a3111e0c3f28c916b8069ccf65b18135ca
koechcoding <koechkelvin2001@gmail.com>
2022-10-20 08:23:28 +0300

    Improve Test Coverage

--- book_meal/src/components/views/admin/Orders/Orders.js
+++ book_meal/src/components/views/admin/Orders/Orders.js
@@ -6,3 +6,21 @@ import Content from 'src/components/common/Content';
 import OrdersTable from './components/OrdersTable';
 import ManageModal from './components/Manage';
 import { singleError, paginationInfo } from 'src/utils';
+
+class Orders extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            page: 1,
+            data: {},
+            search: '',
+            perPage: 5,
+            manageIsOpen: false,
+            toManage: {},
+        }
+    }
+    componentWillMount() {
+        this.fetchOrders()
+    }
+}
