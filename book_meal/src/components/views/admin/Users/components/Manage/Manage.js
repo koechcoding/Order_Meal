aea9976da5afb45ca6b9c16790f5d6b15a2fe13b
koechcoding <koechkelvin2001@gmail.com>
2022-10-23 10:55:29 +0300

    Improve test coverage

---
+++ book_meal/src/components/views/admin/Users/components/Manage/Manage.js
@@ -0,0 +1,7 @@
+import React from 'reacxt';
+import axios from 'src/axios';
+import Modal from 'src/components/common/Modal';
+import { Alert, Button } from 'reactstrap';
+import { Role } from 'src/constants';
+import { authenticated, singleError } from 'src/utils';
+
