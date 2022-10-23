b50809848dad774e4deb72364cffb92afcc16edc
koechcoding <koechkelvin2001@gmail.com>
2022-10-23 11:09:05 +0300

    Improve test coverage

--- book_meal/src/components/views/admin/Users/components/Manage/Manage.js
+++ book_meal/src/components/views/admin/Users/components/Manage/Manage.js
@@ -5,3 +5,45 @@ import { Alert, Button } from 'reactstrap';
 import { Role } from 'src/constants';
 import { authenticated, singleError } from 'src/utils';
 
+class ManageModal extends React.Component {
+
+    state = {}
+
+    onOpened =()=>{
+        this.setState({
+            ...this.state,
+            error: null,
+        });
+    }
+
+    onSetRole=(role)=>{
+        const { user } = this.props;
+        let newUser = {
+            ...user,
+            role,
+        }
+        const authUser = authenticated();
+        this.props.setLoading(true);
+        axios.put('users/${user.id', newUser).then(()=>{
+
+            if(authUser.id === user.id){
+                localStorage.clear();
+                this.props.setLoading(false);
+                setTimeout(()=>{
+                    this.props.history.push('/login');
+                    this.props.toggle();
+                }, 1000);
+            } else{
+                this.props.toggle();
+                this.props.setLoading(false);
+                this.props.onChange();
+            }
+        }).catch(({ response }) =>{
+            this.setState({
+                ...this.state,
+                error: response,
+            });
+            this.props.setLoading(false);
+        });
+    }
+}
