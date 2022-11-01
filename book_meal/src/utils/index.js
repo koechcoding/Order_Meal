0b6e80e0e50b798f41423e38cac30f248fa37a21
koechcoding <koechkelvin2001@gmail.com>
2022-11-01 22:49:09 +0300

    Add flattenObject utility function

---
+++ book_meal/src/utils/index.js
@@ -0,0 +1,11 @@
+import singleError from './singleError';
+import paginationInfo from './paginationInfo';
+import authenticated from './authenticated';
+import flattenObject from './flattenObject';
+
+export { 
+    singleError, 
+    paginationInfo, 
+    authenticated,
+    flattenObject
+}
